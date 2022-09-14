6c75296c92c4c85e45f2d36b5b0211f3b4601cfc
Eяw!и Gontier <ierwinx@me.com>
2022-09-13 19:21:42 -0500

    Initial Commit

---
+++ ShowViews/ShowViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShowViewsApp.swift
+//  ShowViews
+//
+//  Created by Erwin Luz León on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ShowViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
