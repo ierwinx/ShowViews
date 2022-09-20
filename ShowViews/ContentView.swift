2bc652877ecf058c49b5ec70899708d0792d10e6
Eяw!и Gontier <ierwinx@me.com>
2022-09-20 13:39:06 -0500

    se agrega actionSheet y mas opciones a la alerta

--- ShowViews/ContentView.swift
+++ ShowViews/ContentView.swift
@@ -5,6 +5,7 @@ struct ContentView: View {
     @State var bShow = false
     @State var bShow2 = false
     @State var bShow3 = false
+    @State var bShow4 = false
     
     var body: some View {
         VStack(spacing: 30) {
@@ -29,11 +30,21 @@ struct ContentView: View {
             Button {
                 bShow3.toggle()
             } label: {
-                Text("Mostrar vista 2")
+                Text("Mostrar alerta")
                     .padding()
                     .background(.gray)
                     .foregroundColor(.white)
             }
+            
+            Button {
+                bShow4.toggle()
+            } label: {
+                Text("Mostrar ActionSheet")
+                    .padding()
+                    .background(.green)
+                    .foregroundColor(.white)
+            }
+            
         }
         
         .fullScreenCover(isPresented: $bShow) {
@@ -45,7 +56,26 @@ struct ContentView: View {
         }
         
         .alert(isPresented: $bShow3) {
-            Alert(title: Text("Prueba"))
+            Alert(title: Text("Prueba"), message: Text("Soy un contenido"),
+                  primaryButton: Alert.Button.default(Text("No")) {
+                    print("No")
+                  },
+                  secondaryButton: Alert.Button.destructive(Text("Si")) {
+                    print("Si")
+                  }
+            )
+        }
+        
+        .actionSheet(isPresented: $bShow4) {
+            ActionSheet(title: Text("Quieres eliminar los datos?"), message: Text("Esto podra ayudar a restablecer la aplicacion pero se borrara la información"), buttons: [
+                ActionSheet.Button.default(Text("Mas tarde")) {
+                    print("Mas tarde")
+                },
+                ActionSheet.Button.destructive(Text("Eliminar")) {
+                    print("Eliminando")
+                },
+                ActionSheet.Button.cancel()
+            ])
         }
         
     }
